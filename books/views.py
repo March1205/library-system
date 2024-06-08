81cb03f41e5a9212575053aebefb0b8b0edf9d5b
Marharyta Marchenko <ritanika12@gmail.com>
2024-06-08 16:13:58 +0200

    create books viewset

--- books/views.py
+++ books/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from books.models import Book
+from books.serializers import BookSerializer
+from books.permissions import IsAdminOrReadOnly
 
-# Create your views here.
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
