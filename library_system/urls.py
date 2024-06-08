4ddc5f7b522feb0e0a9bf15576aad0a5b9edc419
Marharyta Marchenko <ritanika12@gmail.com>
2024-06-08 18:37:50 +0200

    add swagger URLS for API documentation

--- library_system/urls.py
+++ library_system/urls.py
@@ -16,10 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/books/', include('books.urls', namespace='books')),
     path('api/users/', include('users.urls', namespace='users')),
     path('api/borrowings/', include('borrowings.urls', namespace='borrowings')),
+    path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path(
+        'api/doc/swagger/',
+        SpectacularSwaggerView.as_view(url_name='schema'),
+        name='swagger-ui'
+    ),
 ]
