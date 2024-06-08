a6f4ee5527a377e652f9d6f0e36a9bc349bca6d1
Marharyta Marchenko <ritanika12@gmail.com>
2024-06-08 15:33:59 +0200

    define books model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    HARD = "HARD"
+    SOFT = "SOFT"
+    COVER_CHOICES = [
+        (HARD, "Hard Cover"),
+        (SOFT, "Soft Cover"),
+    ]
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(max_length=4, choices=COVER_CHOICES)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.id} - {self.title}"
