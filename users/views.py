f9b9ed6cb050a3935b4e173e4a87035862df3cb1
Marharyta Marchenko <ritanika12@gmail.com>
2024-06-08 16:38:35 +0200

    create views for user registration, authentication, and profile management

--- users/views.py
+++ users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+from users.serializers import UserSerializer, AuthTokenSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Handles user registration"""
+    serializer_class = UserSerializer
+
+
+class CreateTokenView(TokenObtainPairView):
+    """Handles creating user authentication tokens"""
+    serializer_class = AuthTokenSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Handles retrieving and updating user profile"""
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        """Retrieve and return the authenticated user"""
+        return self.request.user
